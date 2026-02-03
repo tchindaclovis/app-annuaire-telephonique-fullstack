609c1edaea04b1354441412b38730659a5aed4b3
CLOVIS TCHINDA <tchindafonkouclovis@gmail.com>
2026-02-03 16:39:51 +0100

    Commit2-initialisation REACT(Vite)

---
+++ frontend/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+    plugins: [react()],
+    resolve: { extensions: ['.js', '.jsx'] },
+    // server: {
+    //     proxy: {
+    //         '/api': {
+    //             target: 'http://localhost:8080', // ton Spring Boot
+    //             changeOrigin: true
+    //         }
+    //     }
+    // }
+})
+
