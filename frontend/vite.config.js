d1eaf5c8f8821b98c05666b45bb186900d675615
CLOVIS TCHINDA <tchindafonkouclovis@gmail.com>
2026-02-10 14:34:44 +0100

    Commit2-coté backen complet

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     resolve: { extensions: ['.js', '.jsx'] },
-    // server: {
-    //     proxy: {
-    //         '/api': {
-    //             target: 'http://localhost:8080', // ton Spring Boot
-    //             changeOrigin: true
-    //         }
-    //     }
-    // }
+    server: {
+        proxy: {
+            '/api': {  //toutes les requêtes qui commencent par /api vont être redirigées vers ton backend SpringBoot
+                target: 'http://localhost:8080', // ton Spring Boot
+                changeOrigin: true
+            }
+        }
+    }
 })
 
